4283e0574e2bdb52e7333ef7705a8fda6de26b53
Lexi Henson <LexiHensonDev@gmail.com>
2021-10-25 21:17:15 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const PORT = process.env.PORT || 2048;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
