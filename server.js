f84584e047feb90486170e356e531073c3318f22
Lexi Henson <LexiHensonDev@gmail.com>
2021-10-27 05:32:58 -0400

    WIP - Refactoring

--- server.js
+++ server.js
@@ -12,83 +12,103 @@ yup.object({
 let game_running = false
 
 let clients = []
+let playing = []
 let trivia = []
 let trivia_at = ""
 
-const listenForPlayers = (ws) => {
+const z = msg => {
+    if (msg = curQ.correct_answer) {
+        conn.send(true);
+    } else {
+        conn.send(false);
+    }
 
+const validateAnswerListener = (conn, curQ) => {
+    conn.send(JSON.stringify(msg));
+    playing.push(conn.on('message', () => { }))
 }
 
+const gameLoop = () => {
+    const curQ = trivia.shift()
+    const msg = { question: curQ.question, answers: [curQ.correct_answer, ...curQ.incorrect_answers].sort() }
+    playing.forEach(client => {
+        validateAnswerListener(client, msg, curQ)
+    });
 
-wss.on('connection', ws => {
-    clients.push(ws)
-    ws.on('message', async message => {
-        const errHandle = err => {
-            ws.send(err)
-        }
+    game.round += 1
+    if (game.round === rounds) {
+        game_running = false
+        clearInterval(game.gameLoop)
+    }
 
-        let round_length = 3000
-        let rounds = 2
-        const msg = JSON.parse(message.toString())
+    const listenForPlayers = async (ws, msg) => {
+
+        let round_length = 30
+        let rounds = 3
+
+        const msg = JSON.parse(msesage.toString())
         messageSchema.validate(msg).then(async () => {
-            if (msg.cmd == "start_game" && game_running == false) {
-                rounds = msg.rounds ? msg.rounds : rounds
-                round_length = msg.round_length ? msg.round_length : round_length
-                await populate_trivia(rounds, errHandle)
-                ws.send(`Starting game with paramaters: ` + JSON.stringify(msg))
+            if (msg.cmd == 'start_game' && game_running == false) {
+                rounds = msg.rounds ?? rounds;
+                round_length = msg.round_length ?? round_length;
+                await populate_trivia(rounds)
+                ws.send("Starting game with paramaters: \n" + JSON.stringify(msg))
                 game_running = true
-                const gameLoop = () => {
-                    const curQ = trivia.shift()
-                    let curListeners = []
-                    const msg = { question: curQ.question, answers: [curQ.correct_answer, ...curQ.incorrect_answers].sort() }
-                    clients.forEach(conn => {
-                        conn.send(JSON.stringify(msg))
-                        curListeners.push(conn.on('message', msger => {
-                            if (msger = curQ.correct_answer) {
-                                conn.send(true)
-                            } else {
-                                conn.send(false)
-                            }
-                        }))
-                    });
+                playing = [...clients];
 
-                    game.round += 1
-                    if (game.round === rounds) {
-                        game_running = false
-                        clearInterval(game.gameLoop)
+            }
+
+        })
+    }
+
+
+    wss.on('connection', ws => {
+        clients.push(ws)
+        ws.on('message', async message => {
+            const errHandle = err => {
+                ws.send(err)
+            }
+
+            let round_length = 3000
+            let rounds = 2
+            const msg = JSON.parse(message.toString())
+            messageSchema.validate(msg).then(async () => {
+                if (msg.cmd == "start_game" && game_running == false) {
+                    rounds = msg.rounds ? msg.rounds : rounds
+                    round_length = msg.round_length ? msg.round_length : round_length
+                    await populate_trivia(rounds, errHandle)
+                    ws.send(`Starting game with paramaters: ` + JSON.stringify(msg))
+                    game_running = true
+                    let game = {
+                        'round': 0, 'gameLoop': setInterval(gameLoop, round_length)
                     }
+                    gameLoop()
                 }
-                let game = {
-                    'round': 0, 'gameLoop': setInterval(gameLoop, round_length)
-                }
-                gameLoop()
-            }
-            ws.on('close', () => (clients = clients.filter((conn) => (conn === ws ? false : true))))
-        }).catch(err => {
-            clients.forEach(client => {
-                client.send(err)
+                ws.on('close', () => (clients = clients.filter((conn) => (conn === ws ? false : true))))
+            }).catch(err => {
+                clients.forEach(client => {
+                    client.send(err)
+                })
             })
+            ws.send(`Hello, you are connected with ${wss.clients.size - 1} other users!`)
         })
-        ws.send(`Hello, you are connected with ${wss.clients.size - 1} other users!`)
     })
-})
 
-const populate_trivia = async (rounds, errHandle) => {
-    if (trivia_at === '') {
-        await axios.get('https://opentdb.com/api_token.php?command=request').then(result => {
-            trivia_at = result.data.token
-            console.log(trivia_at)
-        })
+    const populate_trivia = async (rounds) => {
+        if (trivia_at === '') {
+            await axios.get('https://opentdb.com/api_token.php?command=request').then(result => {
+                trivia_at = result.data.token
+                console.log(trivia_at)
+            })
+        }
+        if (trivia_at !== "") {
+            await axios.get(`https://opentdb.com/api.php?category=15&amount=${rounds}&token=${trivia_at}`).then(res => {
+                if (res.data.response_code == 0) {
+                    trivia = res.data.results
+                } else {
+                    console.error("error!\n", res.data)
+                    process.exit()
+                }
+            })
+        }
     }
-    if (trivia_at !== "") {
-        await axios.get(`https://opentdb.com/api.php?category=15&amount=${rounds}&token=${trivia_at}`).then(res => {
-            if (res.data.response_code == 0) {
-                trivia = res.data.results
-            } else {
-                errHandle("error! shutting down")
-                console.log("error!")
-                process.exit()
-            }
-        })
-    }
-}
