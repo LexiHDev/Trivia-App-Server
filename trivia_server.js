2571193295e8dc9f6573f2d542aa9b1cbad17542
Lexi Henson <LexiHensonDev@gmail.com>
2021-10-28 09:15:51 -0400

    wip + add eslint

--- trivia_server.js
+++ trivia_server.js
@@ -1,50 +1,85 @@
 const WebSocket = require('ws');
 const yup = require('yup');
 const axios = require('axios');
-
 const messageSchema = yup.object({
-    cmd: yup.string().required().matches(/[a-z]*/),
+  cmd: yup
+      .string()
+      .required()
+      .matches(/[a-z]*/),
+  game: yup.object({
     rounds: yup.number().min(3).max(50).integer(),
-    round_length: yup.number().integer().min(3).max(60)
-})
-const wss = new WebSocket.Server({ port: 1447 })
+    round_length: yup.number().integer().min(3).max(60),
+  }).optional(),
+  answer: yup.number().integer().min(0).max(3).optional(),
+});
+const wss = new WebSocket.Server({port: 1447});
 
-let clients = []
-let playing = []
-let listeners = []
-let trivia = []
-let trivia_at = ""
+const clients = [];
+const playing = [];
+const listeners = [];
+let trivia = [];
+let trivia_at = '';
+const accepting = false;
 
- 
+wss.on('connection', (ws) => {
+  clients.push(ws);
+  ws.on('message', (msg) => {
+    msgHandler(ws, msg);
+  });
+});
 
-wss.on('connection', ws => {
-    clients.push(ws);
-    ws.on('message', )
-})
 
-const startListener = (ws, round_length) => {
-    clients.forEach(cli => {
-        cli.on('message', () => {
-            
-        })
-    })
-}
+const msgHandler = (ws, msg) => {
+  try {
+    console.log('Parsing:', msg.toString());
+    ws.msg = JSON.parse(msg.toString());
+  } catch (err) {
+    ws.send(msg.toString() + ' is invalid JSON');
+    console.error(msg.toString() + ' is invalid JSON');
+    return -1;
+  }
+  console.log(ws.msg);
+  if (messageSchema.isValidSync(msg)) {
+    console.log('Hello');
+    ws.send(ws);
+  }
+};
+
+const startListener = (ws) => {
+  clients.forEach((cli) => {
+    listeners.push(cli.on('message', () => {
+      listenForAnswers(ws);
+    }));
+  });
+};
+
+const listenForAnswers = (ws) => {
+  if (ws.answer == ws.msg.answer) {
+    ws.send('correct!');
+  }
+};
 
 const populate_trivia = async (rounds) => {
-    if (trivia_at === '') {
-        await axios.get('https://opentdb.com/api_token.php?command=request').then(result => {
-            trivia_at = result.data.token
-            console.log(trivia_at)
-        })
-    }
-    if (trivia_at !== "") {
-        await axios.get(`https://opentdb.com/api.php?category=15&amount=${rounds}&token=${trivia_at}`).then(res => {
-            if (res.data.response_code == 0) {
-                trivia = res.data.results
-            } else {
-                console.error("error!\n", res.data)
-                process.exit()
-            }
-        })
-    }
-}
+  if (trivia_at === '') {
+    await axios
+        .get('https://opentdb.com/api_token.php?command=request')
+        .then((result) => {
+          trivia_at = result.data.token;
+          console.log(trivia_at);
+        });
+  }
+  if (trivia_at !== '') {
+    await axios
+        .get(
+            `https://opentdb.com/api.php?category=15&amount=${rounds}&token=${trivia_at}`,
+        )
+        .then((res) => {
+          if (res.data.response_code == 0) {
+            trivia = res.data.results;
+          } else {
+            console.error('error!\n', res.data);
+            process.exit();
+          }
+        });
+  }
+};
