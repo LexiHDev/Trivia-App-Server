ca62779214f61cf43c7b559a28b051daaba6a90b
Lexi Henson <LexiHensonDev@gmail.com>
2021-10-28 12:53:39 -0400

    fix port again

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const gameSchema = yup.object({
   round_length: yup.number().integer().min(3).max(60),
 })
 
-const wss = new WebSocket.Server({ port: 80 });
+const wss = new WebSocket.Server({ process.env.PORT });
 
 let loop = {}
 let clients = [];
